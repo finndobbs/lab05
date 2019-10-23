aea2b01aee6730e6ea875b7f533e969fe933c5af
Finn Dobbs <finndobbs@ku.edu>
2019-10-23 10:30:41 -0500

    Add php variable assignment

---
+++ CreateUser.php
@@ -0,0 +1,13 @@
+<?php
+
+$username = $_POST["username"];
+
+
+echo '<head>';
+echo "<link href='style.css' type='text/css' rel='stylesheet'/>";
+echo '</head>';
+echo '<body>';
+echo '<table>';
+
+
+?>
