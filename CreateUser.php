883635d3bc1e008e840aad4b911954935f2d77b7
Finn Dobbs <finndobbs@ku.edu>
2019-10-23 10:52:26 -0500

    add mysqli

--- CreateUser.php
+++ CreateUser.php
@@ -2,12 +2,19 @@
 
 $username = $_POST["username"];
 
+$mysqli = new mysqli("mysql.eecs.ku.edu", "finndobbs", "ePh9Ecee", "finndobbs");
 
-echo '<head>';
-echo "<link href='style.css' type='text/css' rel='stylesheet'/>";
-echo '</head>';
-echo '<body>';
-echo '<table>';
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
 
+$query = "SELECT user_id, FROM Users, WHERE user_id=" .$username.;
 
+if ($result = $mysqli->query($query)) {
+    // the username is not taken
+    $query = "INSERT INTO Users (" .$username. ")";
+    $mysqli->query($query));
+}
 ?>
