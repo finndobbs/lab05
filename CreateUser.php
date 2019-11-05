31628d6487bebb467d5cfed22e0d4c3b0b57027f
Finn Dobbs <finndobbs@ku.edu>
2019-11-05 15:42:50 -0600

    fix whitespace

--- CreateUser.php
+++ CreateUser.php
@@ -3,7 +3,6 @@
 $username = $_POST["username"];
 
 $mysqli = new mysqli("mysql.eecs.ku.edu", "finndobbs", "ePh9Ecee", "finndobbs");
-
 /* check connection */
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
@@ -13,8 +12,19 @@ if ($mysqli->connect_errno) {
 $query = "SELECT user_id, FROM Users, WHERE user_id=" .$username.;
 
 if ($result = $mysqli->query($query)) {
-    // the username is not taken
+    /* the username is not taken */
     $query = "INSERT INTO Users (" .$username. ")";
     $mysqli->query($query));
+
+    //free result
+    $result->free();
 }
+/* close connection */
+$mysqli->close();
+echo '<head>';
+echo "<link href='style.css' type='text/css' rel='stylesheet'/>";
+echo '</head>';
+echo "<body>";
+echo "<p>line 8</p>";
+echo "</body>";
 ?>
