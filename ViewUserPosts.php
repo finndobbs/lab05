88783c6b80ebd92dafc1a96c13603a4d55e9a9cb
Finn Dobbs <finndobbs@ku.edu>
2019-11-06 07:59:08 -0600

    Add ViewUserPosts.php

---
+++ ViewUserPosts.php
@@ -0,0 +1,26 @@
+<?php
+
+$user = $_POST['username'];
+
+echo '<style>table, th, td { border:1px solid black;}</style>';
+$mysqli = new mysqli("mysql.eecs.ku.edu", "finndobbs", "ePh9Ecee", "finndobbs");
+/* check connection */
+if ($mysqli->connect_error) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$query = "SELECT content FROM Posts WHERE author_id='".$user."'";
+$result = $mysqli->query($query);
+
+echo '<table>';
+echo '<tr><th scope="col" class="col">User</th><th scope="col" class="col">Posts</th></tr>';
+while($row = $result->fetch_assoc()) {
+    echo '<tr>';
+    foreach ($row as $value){
+        echo '<td>'.$value.'</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
+?>
