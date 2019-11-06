2c49ca0f0ea586dcae6349dc9418c75ab09b998f
Finn Dobbs <finndobbs@ku.edu>
2019-11-06 08:43:07 -0600

    Add DeletePosts html page, but with php extention

---
+++ DeletePost_html.php
@@ -0,0 +1,36 @@
+<html>
+	<head>
+		<meta charset='UTF-8'>
+		<link href='style.css' type='text/css' rel='stylesheet'/>
+	</head>
+	<body>
+
+		<form action='ViewUserPosts.php' method='post'>
+			
+			<table>
+            <th scope="col" class="col">Delete</th>
+            <th scope="col" class="col">Post ID</th>
+            <th scope="col" class="col">User</th>
+            <th scope="col" class="col">Post</th>
+
+            <?php
+                $mysqli = mysqli_connect("mysql.eecs.ku.edu", "finndobbs", "ePh9Ecee", "finndobbs") or die('error');
+                $query = "SELECT * from Posts";
+                $result = mysqli_query($mysqli, $query) or die('Error querying database');
+
+                while ($row = mysqli_fetch_assoc($result)){
+                    echo '<tr><td><input type="checkbox" name="post" value="'.$row["post_id"].'"></td>';
+                    echo '<td>'.$row["post_id"].'</td>';
+                    echo '<td>'.$row["author_id"].'</td>';
+                    echo '<td>'.$row["content"].'</td></tr>';
+
+                }
+                mysqli_close($mysqli);
+            ?>
+			</table>
+
+			<input type='submit'>
+			<input type='reset'>
+		</form>
+	</body>
+</html>
